3c438278d8f085b70a2eecddb2e025ccbe357d40
EmirHazir <emircanaziz@gmail.com>
2017-08-12 20:34:32 +0300

    listed category and detailed books

--- Library/Controllers/CatalogController.cs
+++ Library/Controllers/CatalogController.cs
@@ -26,6 +26,7 @@ namespace Library.Controllers
                 {
                     Id = result.Id,
                     ImgUrl = result.ImageUrl,
+                    DeweyCallNumber = _asset.GetDeweyIndex(result.Id),
                     AuthorOrDirector = _asset.GetAuthorOrdirectory(result.Id),
                     Title = result.Title,
                     Type = _asset.GetType(result.Id)
@@ -39,7 +40,26 @@ namespace Library.Controllers
             return View(model);
         }
 
+        public IActionResult Detail(int id)
+        {
+            var asset = _asset.GetById(id);
 
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl =asset.ImageUrl,
+                AuthorOrDirector = _asset.GetAuthorOrdirectory(id),
+                CurrentLocation = _asset.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _asset.GetDeweyIndex(id),
+                ISBN = _asset.GetISBN(id)
+            };
+
+            return View(model);
+        }
 
     }
 }
